61290beb7983b6047cee0e1764fea7997dd72e1e
kwonjuyoung <20161527@vision.hoseo.edu>
2023-08-10 12:30:06 +0900

    DTO, Controller

--- backend/community/src/main/java/com/teamhs/community/controller/BoardController.java
+++ backend/community/src/main/java/com/teamhs/community/controller/BoardController.java
@@ -3,34 +3,40 @@ package com.teamhs.community.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/*
+게시판
+1. /board/write : 게시글 작성
+2. /board/list : 게시글 리스트 조회
+3. /board/detail : 게시글 상세 조회
+4. /board/delete : 게시글 삭제
+5. /board/update : 게시글 수정
+
+ */
+
+
 @Controller
 public class BoardController {
 
-    //게시판 작성
     @GetMapping("/board/write")
     public String boardWrite(){
         return "boardwrite";
     }
 
-    //게시판 리스트 조회
     @GetMapping("/board/list")
     public String boardList() {
         return "boardlist";
     }
 
-    //게시판 상세조회
     @GetMapping("/board/detail")
     public String boardDetail() {
         return "boardlist";
     }
 
-    //게시판 수정
     @GetMapping("/board/update")
     public String boardUpdate() {
         return "boardlist";
     }
 
-    //게시판 삭제
     @GetMapping("/board/delete")
     public String boardDelete() {
         return "boardlist";

06ad96410cc2a43954e983e0f76920a8790ee016
kwonjuyoung <20161527@vision.hoseo.edu>
2023-08-10 12:31:54 +0900

    DTO, Controller

--- backend/community/src/main/java/com/teamhs/community/controller/BoardController.java
+++ backend/community/src/main/java/com/teamhs/community/controller/BoardController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 게시판
 1. /board/write : 게시글 작성
 2. /board/list : 게시글 리스트 조회
-3. /board/detail : 게시글 상세 조회
-4. /board/delete : 게시글 삭제
-5. /board/update : 게시글 수정
+3. /board/detail/{c_id} : 게시글 상세 조회
+4. /board/delete/{c_id} : 게시글 삭제
+5. /board/update/{c_id} : 게시글 수정
 
  */
 

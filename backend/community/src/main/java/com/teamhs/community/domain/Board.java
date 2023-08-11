23a4a3f343dd9bae1da9ddf0e1abe2fe54e429da
kwonjuyoung <20161527@vision.hoseo.edu>
2023-08-11 17:45:21 +0900

    list 불러오기

--- backend/community/src/main/java/com/teamhs/community/domain/Board.java
+++ backend/community/src/main/java/com/teamhs/community/domain/Board.java
@@ -3,11 +3,19 @@ package com.teamhs.community.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.sql.Date;
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "community")
+@Getter
+@Setter
 public class Board {
     @Id
     private String user_id;
@@ -28,8 +36,12 @@ public class Board {
     @Column(nullable = true)
     private int cm_cnt;
 
+    public Board(){
+
+    }
+
     //requestDto가 접근, Domain을 통해 db를 Get할 수 있도록 정의
-    public Board(String user_id, int c_id, int cate_id, String c_title, String b_content, LocalDate c_date, int c_recom, int c_cnt, int cm_cnt){
+    public Board(String user_id, int c_id, int cate_id, String c_title, String c_content, LocalDate c_date, int c_recom, int c_cnt, int cm_cnt){
         super();
         this.user_id = user_id;
         this.c_id = c_id;

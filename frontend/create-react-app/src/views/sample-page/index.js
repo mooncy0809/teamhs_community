f57f39d74d598232ff2c903114c0460a1d2503e8
kwonjuyoung <20161527@vision.hoseo.edu>
2023-08-15 18:04:38 +0900

    - 게시글 상세 조회 - 페이징 처리 완료

--- frontend/create-react-app/src/views/sample-page/index.js
+++ frontend/create-react-app/src/views/sample-page/index.js
@@ -1,4 +1,4 @@
-import { Grid, Button} from '@mui/material';
+import { Grid, Button, Pagination} from '@mui/material';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
@@ -11,13 +11,26 @@ import Table from 'react-bootstrap/Table';
 import {useNavigate } from 'react-router-dom';
 
 const BoardList = () => {
-  const [boardlist , setBoardList] = useState([]); 
+  const [boardlist, setBoardList] = useState([]);
+
+  //페이징 기능 구현
+  const [currentPage, setCurrentPage] = useState(0); // Current page state
+  const [totalPages, setTotalPages] = useState(0); // Total pages state
 
   useEffect(() => {
-    axios.get('http://localhost:8090/board/list')
-      .then(response => setBoardList(response.data)) 
-      .catch(error => console.log(error))
-  }, []);
+    const fetchBoardData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8090/board/list?page=${currentPage}&size=15`);
+        setBoardList(response.data.content);
+        setTotalPages(response.data.totalPages);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    fetchBoardData();
+  }, [currentPage]);
+
 
   const navigate = useNavigate(); // useNavigate 함수 가져오기
 
@@ -31,13 +44,19 @@ const BoardList = () => {
     navigate(`/sample-page/boardwatch/${c_id}`); // 해당 경로로 페이지 이동
   };
 
+  //페이징 기능 구현
+  const handlePageChange = (event, page) => {
+    setCurrentPage(page - 1);
+  };
+
   
   return (
     <MainCard title={<span style={{ fontSize: '24px', fontWeight: 'bold' }}>커뮤니티</span>} style={{ marginLeft: '8px' }} secondary={<Button variant="contained" onClick={handleButtonClick} style={{ marginRight: '8px' }}>게시글 작성</Button>}>
       <Grid container spacing={gridSpacing}>
         <Grid item xs={12} sm={12}>
           <SubCard>
-            <Table bordered hover size="sm" style = {{minHeight : '500px'}} >
+            {boardlist.length > 0 ? (
+            <Table bordered hover size="sm" style = {{minHeight : '100%'}} >
               <thead>
                 <tr >
                   <th style={{ width: '5%', textAlign: 'center' , backgroundColor: '#f5f5f5' }}>번호</th>
@@ -59,7 +78,15 @@ const BoardList = () => {
                 ))}
               </tbody>
             </Table>
+             ) : (
+              <p>Loading...</p>
+              )}
           </SubCard>
+          <Pagination
+          count={totalPages}
+          page={currentPage + 1} // Page numbers are 1-based
+          onChange={handlePageChange}
+        />
         </Grid>
       </Grid>
     </MainCard>

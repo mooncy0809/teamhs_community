9cba29d1ef375a24fd303c096642ae25cb37bc98
youngho <mmy4637@naver.com>
2023-08-16 23:50:18 +0900

    SignUp Commit

---
+++ backend/community/src/main/java/com/teamhs/community/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.teamhs.community.repository;
+
+import com.teamhs.community.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, String> {
+}
